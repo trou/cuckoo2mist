85c8251d12ba539f001cbed9a977ce60f60576d1
trinius <trinius@43e4038d-e63a-47a1-9d35-469ca9b921ab>
2013-11-17 20:07:24 +0000

    -- License added -- Update on cuckoo_elements2mist (by Paolo Di Prodi) -- ProgressBar for learn_cuckoo   (by Paolo Di Prodi)

--- mistSplit.py
+++ mistSplit.py
@@ -4,8 +4,24 @@
 mistSplit.py
 
 Created by Philipp Trinius on 2013-11-10.
-Copyright (c) 2013 pi-one.net . All rights reserved.
+Copyright (c) 2013 pi-one.net . 
+
+This program is free software; you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by the 
+Free Software Foundation; either version 2 of the License, or (at your option) 
+any later version.
+
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+more details.
+
+You should have received a copy of the GNU General Public License along with 
+this program; if not, see <http://www.gnu.org/licenses/>
 """
+
+__author__ = "philipp trinius"
+__version__ = "0.2"
 
 import ntpath
 
