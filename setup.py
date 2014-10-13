2d11c4032e5c393536e451a0c4a870949f4957c2
Raphaël Rigo <raphael.rigo@airbus.com>
2014-10-13 15:13:44 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = 'cuckoo2mist',
+    version = '0.2',
+    packages=['cuckoo2mist'],
+    # Metadata
+    author = 'Philipp Trinius'
+)
